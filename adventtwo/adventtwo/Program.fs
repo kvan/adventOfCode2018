47da3e82ec87c74f0e84a8a31630d4b00f6403f8
Casper Kvan Clausen <ckc@kvans.org>
2018-12-02 10:59:47 -0600

    Actually commit things

---
+++ adventtwo/adventtwo/Program.fs
@@ -0,0 +1,42 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+namespace AdventTwo
+
+module ProblemOne =
+
+    let letterDistributions (s:string) =
+        s.ToCharArray()
+        |> Seq.groupBy(fun c -> c)
+        |> Map.ofSeq
+        |> Map.map(fun k v -> Seq.length v)
+    
+    let hasNumber (s:string) (n:int) =
+        letterDistributions s
+        |> Map.tryPick(fun k v -> if v = n then Some(k) else None)
+
+    let checksum (boxes) =
+        let twos = boxes |> Seq.map(fun s -> hasNumber s 2) |> Seq.choose id
+        let threes = boxes |> Seq.map(fun s -> hasNumber s 3) |> Seq.choose id
+        Seq.length twos * Seq.length threes
+
+open System.IO
+open ProblemOne
+
+module Main =
+    [<EntryPoint>]
+    let main argv = 
+        match argv with
+        | [||] -> 
+            printfn "Need a filename."
+            1
+        | [| x |] ->
+            if File.Exists x then
+                let lines = File.ReadAllLines x
+                printfn "Checksum is %i" (checksum lines)
+                0 // return an integer exit code
+            else
+                printfn "File not found: %s" x
+                1
+        | _ ->
+            printfn "Need a filename only."
+            1
---
+++ adventtwo/tests/Tests.fs
@@ -0,0 +1,20 @@
+module Tests
+
+open System
+open Xunit
+open AdventTwo
+
+[<Fact>]
+let ``hasNumber finds something`` () =
+    Assert.Equal(Some 'a', ProblemOne.hasNumber "aab" 2)
+    Assert.Equal(Some 'b', ProblemOne.hasNumber "aabbb" 3)
+    Assert.Equal(Some 'a', ProblemOne.hasNumber "aabb" 2)
+
+[<Fact>]
+let ``hasNumber finds nothing`` () =
+    Assert.Equal(None, ProblemOne.hasNumber "abc" 2)
+
+[<Fact>]
+let ``checksum`` () =
+    let boxes = [| "abcdef"; "bababc"; "abbcde"; "abcccd"; "aabcdd"; "abcdee"; "ababab" |]
+    Assert.Equal(12, ProblemOne.checksum boxes)
